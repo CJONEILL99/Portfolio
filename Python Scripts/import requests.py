9cec183c78bb6f57e8173901d12bfc1adb192387
Conor O'Neill <150390078+CJONEILL99@users.noreply.github.com>
2023-11-23 15:35:51 +0000

    Add files via upload

---
+++ Python Scripts/import requests.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_user = input('Input GitHub user: ')
+url = 'https://github.com/'+github_user #will find the user page of whatever username the user inputs
+r = requests.get(url)
+soup = bs(r.content, 'html.parser')
+profile_image = soup.find('img', 'alt') #scrapes page HTML code to find the specific img tag we want, also getting the src attribute
+print(profile_image)
