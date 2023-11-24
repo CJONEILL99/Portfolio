0ec3754d3e191a102d80467ae03932d053966704
Conor O'Neill <150390078+CJONEILL99@users.noreply.github.com>
2023-11-24 15:47:08 +0000

    Update import requests.py

--- Python Scripts/import requests.py
+++ Python Scripts/import requests.py
@@ -5,5 +5,5 @@ github_user = input('Input GitHub user: ')
 url = 'https://github.com/'+github_user #will find the user page of whatever username the user inputs
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
-profile_image = soup.find('img', 'alt')[scr]#scrapes page HTML code to find the specific img tag we want, also getting the src attribute
+profile_image = soup.find('img')[scr]#scrapes page HTML code to find the specific img tag we want, also getting the src attribute
 print(profile_image)
