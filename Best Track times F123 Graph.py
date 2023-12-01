042b8d574af16b41e6ebe18c3d0373b9bd82de94
Conor O'Neill <150390078+CJONEILL99@users.noreply.github.com>
2023-12-01 14:35:39 +0000

    Update Best Track times F123 Graph.py

--- Best Track times F123 Graph.py
+++ Best Track times F123 Graph.py
@@ -1,10 +1,14 @@
 #Best Track times F123 Graph
-python3 -mpip matplotlib as plt
+
+import matplotlib.pyplot as plt
 import pandas as pd
 
 best_times_per_track = pd.read_csv('F1_23_Lap_times.csv')
 
 plt.plot(best_times_per_track['Track'],
-         best_times_per_track['Fastest_lap'])
+         best_times_per_track['Lap_in_seconds']) #TypeError: 'Value' must be an instance of str or bytes, not a float.
+
+plt.xlabel('Tracks')
+plt.ylabel('Fastest Time in seconds')
 
 plt.show()
